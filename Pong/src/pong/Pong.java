e35b550106c053b9522fed57449cb63ee4229ba3
TehSuzzles <suziepaulin@gmail.com>
2020-11-13 12:29:59 +1300

    Update

--- Pong/src/pong/Pong.java
+++ Pong/src/pong/Pong.java
@@ -3,8 +3,8 @@ import javax.swing.JFrame;
 public class Pong extends JFrame {
 	
 	private final static String WINDOW_TITLE = "Pong";
-	private final static int WINDOW_WIDTH = "800";
-	private final static int WINDOW_HEIGHT = "600";
+	private final static int  WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
 	
 	
 	public Pong() {
@@ -13,13 +13,15 @@ public class Pong extends JFrame {
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		add(new PongPanel());
 	}
 	
 	
 
 	public static void main(String[] args) {
 		
-new Pong ();
+			new Pong ();
+			
 	}
 
 }
